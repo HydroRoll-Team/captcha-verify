e561ad827070ddd748906c1787ec568a751a4ae9
简律纯 <i@jyunko.cn>
2024-01-19 11:46:50 +0800

    refactor(api.index): test for env

--- api/index.py
+++ api/index.py
@@ -1,24 +1,11 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-@app.route("/about")
+@app.route('/about')
 def about():
-    return "About"
-
-
-@app.route("/<path:url>")
-def display_external_content(url):
-    try:
-        response = requests.get(url)
-
-        if response.status_code != 200:
-            return f"Failed to retrieve content from {url}. Status code: {response.status_code}"
-
-        return render_template("display.html", content=response.text)
-    except Exception as e:
-        return f"An error occurred: {str(e)}"
-    
-app.run()
+    return 'About'
